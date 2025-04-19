c6ed2d6d7d3fcaa428a1fbc47c1dd3c259ec89ff
jhd66g <122474505+jhd66g@users.noreply.github.com>
2025-04-19 04:37:32 +0000

    hardcode the api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
 // src/api.js
-export const API = process.env.REACT_APP_API_URL || '';
+export const API = process.env.REACT_APP_API_URL || 'https://stinger-env.eba-7vf85uj7.us-west-2.elasticbeanstalk.com';
